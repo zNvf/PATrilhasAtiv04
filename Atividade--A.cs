0dc70b07957bfb902f489eeea4cf508c1a657e23
zNvf <tradelost2@gmail.com>
2025-02-19 09:14:00 -0300

    Create Atividade--A.cs

---
+++ Atividade--A.cs
@@ -0,0 +1,16 @@
+using System;
+namespace PrimeiraAtividade
+{
+class Program
+{
+public static void Main()
+{
+Console.Write("Digite um número : ");
+int numero = Convert.ToInt32(Console.ReadLine());
+string resposta = (numero % 2 == 0) ? "Par" : "Ímpar";
+Console.WriteLine("O número " + numero + " é " + resposta);
+Console.WriteLine("\n\rAperte alguma tecla para fechar...");
+Console.ReadKey(true);
+}
+}
+}
